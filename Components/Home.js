321d6fb270659e0bdc845f8893382f8fe274f1b3
Sabarinath <62168230+sab30-webdev@users.noreply.github.com>
2023-04-06 18:28:25 +0530

    UI changes

--- Components/Home.js
+++ Components/Home.js
@@ -159,6 +159,10 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     elevation: 5,
+    borderRadius: 15,
+    marginLeft: 10,
+    marginRight: 10,
+    marginBottom: 10,
   },
   top: {
     backgroundColor: "white",
